dbc6cf66a871a3cbdb986a8188577b6e51ef0919
gventurini <giacomo.venturini3@gmail.com>
2022-06-12 17:00:31 +0200

    add comments

--- pip-install-and-save.sh
+++ pip-install-and-save.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# install and save python library to requirement.txt file
 function pipis {
   local PYTHON_LIB=$1
   if [ -z "$2" ]
   then
     local REQUIREMENTS_FILE=requirements.txt
   else
-    local REQUIREMENTS_FILE=$2
+    local REQUIREMENTS_FILE=$2 # custom requirements
   fi
   pip install "$PYTHON_LIB" && pip freeze | grep "$PYTHON_LIB" >> "$REQUIREMENTS_FILE"
 }
